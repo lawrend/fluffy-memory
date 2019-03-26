58a2d8fff0e43474e848832e100401c27cf63aca
lawrend <douglas_lawrence@mac.com>
2019-03-26 15:21:37 -0700

    adding fallback route for use with react router

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
     resources :countries, :country_years, :years
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
