51f6178b30fd3ca8806ecfe0e6a2ccedd4e46934
lawrend <douglas_lawrence@mac.com>
2019-03-28 04:35:43 -0700

    get enpoints for year contract data

--- app/models/year.rb
+++ app/models/year.rb
@@ -3,4 +3,7 @@ class Year < ApplicationRecord
   has_many :countries, through: :country_years
   has_many :species_years
   has_many :species, through: :species_years
+
+
+  # need to get endpoints for all contract datasets for example https://public.enigma.com/datasets/government-spending-contracts-2018/0c24ef38-9a7c-4bd2-98ec-c32ac9916aaf
 end
