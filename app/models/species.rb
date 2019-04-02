cdbac2267f358030fcce0310d747089e09da6b8a
lawrend <douglas_lawrence@mac.com>
2019-04-02 16:50:54 -0700

    added concern for shared enigma call methods for use amongst models

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,4 +1,5 @@
 class Species < ApplicationRecord
+  include Enigma
 
   def self.get_enigma_collections
     conn = Faraday.new "https://public.enigma.com/api/"
@@ -11,19 +12,19 @@ class Species < ApplicationRecord
     # @enigma = resp.body["current_snapshot"]["id"]
   end
  
-  def self.get_enigma_dataset(datasetID)
-    conn = Faraday.new "https://public.enigma.com/api/"
-    resp = conn.get("datasets/#{datasetID}") do |req|
-      req.headers['Authorization'] = "Bearer: #{ENV['ENIGMA_API_KEY']}"
-    end
-    snapshot = JSON.parse(resp.body)
-    @snapshotID = snapshot['current_snapshot']['id']
+  # def self.get_enigma_dataset(datasetID)
+  #   conn = Faraday.new "https://public.enigma.com/api/"
+  #   resp = conn.get("datasets/#{datasetID}") do |req|
+  #     req.headers['Authorization'] = "Bearer: #{ENV['ENIGMA_API_KEY']}"
+  #   end
+  #   snapshot = JSON.parse(resp.body)
+  #   @snapshotID = snapshot['current_snapshot']['id']
 
-    resp2 = conn.get("snapshots/#{@snapshotID}?&row_limit=200&row_offset=0&include_serialids=true") do |req|
-      req.headers['Authorization'] = "Bearer: #{ENV['ENIGMA_API_KEY']}"
-    end
+  #   resp2 = conn.get("snapshots/#{@snapshotID}?&row_limit=200&row_offset=0&include_serialids=true") do |req|
+  #     req.headers['Authorization'] = "Bearer: #{ENV['ENIGMA_API_KEY']}"
+  #   end
 
-    @dataset = JSON.parse(resp2.body)
-  end
+  #   @dataset = JSON.parse(resp2.body)
+  # end
 
 end
