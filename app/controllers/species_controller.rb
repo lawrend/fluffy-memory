07a51b768f8cbf9e4d9ee93191da3e1bf166fa99
lawrend <douglas_lawrence@mac.com>
2019-04-09 16:26:32 -0700

    cleaned up index call

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -5,21 +5,19 @@ class SpeciesController < ApiController
   # GET /species
   # either get all the species/locations records or make them
   def index
-    if Species.all.count > 1
-      @species = Species.all
-      render json: @species
-    else
-      @species = Species.get_enigma_dataset("f2778fbc-47fd-45e3-a01a-936040650096")
-      @rows = @species['table_rows']['rows']
+
+    if Species.all.count < 2
+      @new_species = Species.get_enigma_dataset("f2778fbc-47fd-45e3-a01a-936040650096")
+      @rows = @new_species['table_rows']['rows']
       @names = @rows.collect {|r| r[1,5]}
       @names.each do |n| 
-        @species = Species.find_or_create_by(name: n[0], status: n[1] )
+        @animal = Species.find_or_create_by(name: n[0], status: n[1] )
         @location = Location.find_or_create_by(loc: n[2], state: n[3], other_states: n[4])
-        SpeciesLocation.find_or_create_by(species_id: @species.id, location_id: @location.id)
+        SpeciesLocation.find_or_create_by(species_id: @animal.id, location_id: @location.id)
       end
-
-      render json: @names
     end
+    @species = Species.all
+    render json: @species
   end
 
   # GET /species/1
