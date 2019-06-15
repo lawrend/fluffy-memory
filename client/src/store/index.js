6b142a7260c48ba155bf5c9b8211a4aa952a9abe
lawrend <douglas_lawrence@mac.com>
2019-06-15 13:03:55 -0700

    changed file name to rootReducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,7 @@
 // CREATES STORE: importing the devtools, createStore, applyMiddleware, rootReducer, and thunk--all to create the store
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { createStore, applyMiddleware } from 'redux';
-import {rootReducer} from './reducers/index.js';
+import { rootReducer } from './reducers/rootReducer.js';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger'
 
