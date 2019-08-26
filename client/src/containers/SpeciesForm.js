64904b7b3a5c9cbafa71ab2baaae682833e1edfa
lawrend <douglas_lawrence@mac.com>
2019-08-26 12:17:14 -0700

    small cosmetics post project call

--- client/src/containers/SpeciesForm.js
+++ client/src/containers/SpeciesForm.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import SpeciesData from '../components/SpeciesData.js';
-import axios from 'axios';
-import { Card, Divider } from 'semantic-ui-react';
+//import React, { Component } from 'react';
+//import SpeciesData from '../components/SpeciesData.js';
+//import axios from 'axios';
+//import { Card, Divider } from 'semantic-ui-react';
 
-//species are stored with a name, id, and status
-//not currently used
-class SpeciesForm extends Component {
-  state = {
-    species: [],
-  }
+////species are stored with a name, id, and status
+////not currently used
+//class SpeciesForm extends Component {
+//  state = {
+//    species: [],
+//  }
 
-  componentDidMount() {
-    axios.get('/api/species')
-      .then(resp => {
-        const species = resp.data;
-        this.setState({species})
-      })
-  }
+//  componentDidMount() {
+//    axios.get('/api/species')
+//      .then(resp => {
+//        const species = resp.data;
+//        this.setState({species})
+//      })
+//  }
 
-  render(){
-    console.log(this.state.species)
-    return(
-      <div className="homepage">
-          this is the species data container
-        <Divider />
-        <Card.Group centered>
-          <SpeciesData species={this.state.species}/>
-        </Card.Group>
-      </div>
-        )
-  }
-}
+//  render(){
+//    console.log(this.state.species)
+//    return(
+//      <div className="homepage">
+//          this is the species data container
+//        <Divider />
+//        <Card.Group centered>
+//          <SpeciesData species={this.state.species}/>
+//        </Card.Group>
+//      </div>
+//        )
+//  }
+//}
 
-export default SpeciesForm
+//export default SpeciesForm
